cc2c0aa8083c5bb648ad252afed039414a83322e
Siarhei Rylach <Siarhei_Rylach@epam.com>
2018-07-07 16:57:56 +0300

    done

--- test/readData.spec.js
+++ test/readData.spec.js
@@ -1,35 +1,42 @@
 'use strict';
 
 const path = require('path');
-// instead of relative path with '../../'
-// it is better to use path.resolve
-//const readData = require('../lib/readData');
 const readData = require(path.resolve('lib/readData'));
+const chai =  require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 describe('readData', () => {
 
     it('should read data from from properties file', () => {
-        let data = readData('somefile.properties');
-        expect(data).toBe('data from properties file');
+        let data = readData('test/test-data/test.properties');
+        expect(data).to.eventually.to.have.property('a', 1);
+        expect(data).to.eventually.to.have.property('b', 2);
     });
 
     it('should read data from from csv file', () => {
-        let data = readData('somefile.csv');
-        expect(data).toBe('data from csv file');
+        let data = readData('test/test-data/test.csv');
+        expect(data).to.eventually.to.have.property('id', '1');
+        expect(data).to.eventually.to.have.property('user', 'foo');
+        expect(data).to.eventually.to.have.property('pass', '123');
     });
 
-    it('should read data from from json file', () => {
-        let data = readData('test/test-data/test.json');
-        expect(data.data).toBe('data from json file');
-    });
-
+     it('should read data from from json file', () => {
+         let data = readData('test/test-data/test.json');
+         expect(data.data).to.equal('data from json file');
+     });
+    //
     it('should read data from from yml file', () => {
-        let data = readData('somefile.yml');
-        expect(data).toBe('data from yml file');
+        let data = readData('test/test-data/test.yml');
+        expect(data).to.have.property('environment', 'production');
+        expect(data).to.have.property('parameters', null);
     });
 
     it('should read data from from xlsx file', () => {
-        let data = readData('somefile.xlsx');
-        expect(data).toBe('data from xlsx file');
+        let data = readData('test/test-data/test.xlsx', {sheetName: 'Sheet1'});
+        expect(data.A1.v).to.equal(1);
+        expect(data.B1.v).to.equal(2);
+        expect(data.C1.v).to.equal(3);
     });
 });
